7059debb3b28c5f6096e48dc45e1f8e740e3e071
Md. Nakib Hasan Emon <emon15-5847@diu.edu.bd>
2025-09-17 16:49:29 +0600

    radiolistile

--- lib/main.dart
+++ lib/main.dart
@@ -28,22 +28,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PageController _controller = PageController(initialPage: 0);
-
-  @override
-  void dispose() {
-    _controller.dispose(); // prevent memory leaks
-    super.dispose();
+  int _rvalue = 0;
+  void method(int value) {
+    setState(() {
+      _rvalue = value;
+    });
   }
-
-  bool isChecked = false; // state variable
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
-    var orientation = MediaQuery.of(context).orientation;
-
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.home, color: Colors.white),
@@ -58,28 +51,59 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Radio(
-              value: 1,
-              groupValue: 1,
-              activeColor: Colors.lightBlue,
-              onChanged: (value) {},
-            ),
-            Radio(
-              value: 2,
-              groupValue: 1,
-              activeColor: Colors.lightBlue,
-              onChanged: (value) {},
-            ),
-            Radio(
-              value: 3,
-              groupValue: 1,
-              activeColor: Colors.lightBlue,
-              onChanged: (value) {},
-            ),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.lightBlue),
+          ),
+          child: Column(
+            children: [
+              RadioListTile(
+                value: 1,
+                groupValue: _rvalue,
+                activeColor: Colors.lightBlue,
+
+                title: Text("iphone"),
+                subtitle: Text("data"),
+                secondary: Icon(Icons.apple),
+                controlAffinity: ListTileControlAffinity.trailing,
+                selected: _rvalue == 1 ? true : false,
+
+                onChanged: (value) {
+                  method(value!);
+                },
+              ),
+              RadioListTile(
+                value: 2,
+                groupValue: _rvalue,
+                activeColor: Colors.lightBlue,
+                title: Text("samsung"),
+                subtitle: Text("data"),
+                secondary: Icon(Icons.phone_sharp),
+                controlAffinity: ListTileControlAffinity.trailing,
+                selected: _rvalue == 2 ? true : false,
+                onChanged: (value) {
+                  method(value!);
+                },
+              ),
+              RadioListTile(
+                value: 3,
+                groupValue: _rvalue,
+                activeColor: Colors.lightBlue,
+
+                title: Text("Nokia"),
+                subtitle: Text("data"),
+                secondary: Icon(Icons.handshake_sharp),
+                controlAffinity: ListTileControlAffinity.trailing,
+                selected: _rvalue == 3 ? true : false,
+
+                onChanged: (value) {
+                  method(value!);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
