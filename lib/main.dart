171814033d25156ded01951306de6c11d33cf1f6
Md. Nakib Hasan Emon <emon15-5847@diu.edu.bd>
2025-09-20 14:56:41 +0600

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:testproject/page1.dart';
-import 'package:testproject/page2.dart';
-import 'package:testproject/page3.dart';
-import 'package:testproject/style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,21 +24,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _rvalue = 2;
-  bool _svalue = false;
-  void method(int value) {
-    setState(() {
-      _rvalue = value;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.lightBlue, Colors.deepPurple],
+            ),
+          ),
+        ),
         leading: const Icon(Icons.home, color: Colors.white),
         title: const Text("Appbar", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.lightBlue,
         centerTitle: true,
         actions: [
           IconButton(
@@ -54,41 +46,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 200,
-            backgroundColor: Colors.red,
-            flexibleSpace: FlexibleSpaceBar(title: Text("SliverAppbar")),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              addDetails("One", "This is one"),
-              addDetails("Two", "This is two"),
-              addDetails("Three", "This is three"),
-              addDetails("Four", "This is one"),
-              addDetails("Five", "This is two"),
-              addDetails("Six", "This is three"),
-              addDetails("Seven", "This is one"),
-              addDetails("Eight", "This is two"),
-              addDetails("Nine", "This is three"),
-              addDetails("Ten", "This is one"),
-              addDetails("Twelve", "This is two"),
-              addDetails("Thirteen", "This is three"),
-            ]),
-          ),
-        ],
-      ),
+      body: Center(child: Text("data")),
     );
   }
-}
-
-Widget addDetails(String name, String description) {
-  return ListTile(
-    title: Text(name),
-    tileColor: const Color.fromARGB(255, 82, 192, 255),
-    subtitle: Text(description),
-    leading: CircleAvatar(child: Text(name[0])),
-  );
 }
