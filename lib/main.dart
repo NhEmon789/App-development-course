ea02bac90caa55f354a018dd3c0852c5c5a7dacf
Md. Nakib Hasan Emon <emon15-5847@diu.edu.bd>
2025-09-21 20:15:53 +0600

    toast message

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() => runApp(MaterialApp(home: BottomNavBar()));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: BottomNavBar());
+  }
+}
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
   @override
-  _BottomNavBarState createState() => _BottomNavBarState();
+  State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  final pages = [A(), B(), C(), D()];
+  void _showToast() {
+    Fluttertoast.showToast(
+      msg: "Easy Explanation",
+      toastLength: Toast.LENGTH_SHORT, // SHORT is more common
+      gravity: ToastGravity.BOTTOM, // TOP / CENTER / BOTTOM
+      timeInSecForIosWeb: 2, // iOS/web requires this for duration
+      backgroundColor: Colors.lightBlue,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("Fluttertoast Example")),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return Dialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Stack(
-                    clipBehavior: Clip.none, // <-- important (allows overflow)
-                    alignment: Alignment.topCenter,
-                    children: [
-                      SizedBox(
-                        height: 250,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              SizedBox(height: 50),
-                              Text(
-                                "Alert Dialog",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 23,
-                                  color: Color(0xff070707),
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                "This is an Alert Dialog",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 14,
-                                  color: Color(0xff070707),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: -30, // makes avatar float outside dialog
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.white,
-                          child: ClipOval(
-                            child: Image.asset(
-                              "images/ph1.jpg",
-                              fit: BoxFit.cover,
-                              width: 60,
-                              height: 60,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          },
-          child: const Text("click"),
+          onPressed: _showToast,
+          child: const Text("Click me"),
         ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        index: 0,
-        color: Colors.white,
-        backgroundColor: Colors.blue,
-        items: const [
-          Icon(Icons.home),
-          Icon(Icons.subject),
-          Icon(Icons.group),
-          Icon(Icons.settings),
-        ],
       ),
     );
   }
 }
-
-class D {}
-
-class C {}
-
-class B {}
-
-class A {}
