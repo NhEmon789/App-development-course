60e12a89e01562dbf638e004273fe305d843a57a
Md. Nakib Hasan Emon <emon15-5847@diu.edu.bd>
2025-09-23 23:23:57 +0600

    datatable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,74 +21,34 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  List? data;
-  bool isLoading = true;
-
-  Future<void> getData() async {
-    try {
-      var response = await http.get(
-        Uri.parse("https://jsonplaceholder.typicode.com/users"),
-      );
-
-      if (response.statusCode == 200) {
-        var decode = jsonDecode(response.body);
-        setState(() {
-          data = decode;
-          isLoading = false;
-        });
-      } else {
-        setState(() {
-          isLoading = false;
-        });
-        debugPrint("Server error: ${response.statusCode}");
-      }
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-      });
-      debugPrint("Error fetching data: $e");
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-
-    if (data == null || data!.isEmpty) {
-      return const Scaffold(body: Center(child: Text("No data available")));
-    }
-
     return Scaffold(
-      appBar: AppBar(title: const Text("Users")),
-      body: ListView.builder(
-        itemCount: data!.length,
-        itemBuilder: (context, index) {
-          var user = data![index];
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.blueAccent,
-              child: Text(user["name"][0]), // First letter of name
+      appBar: AppBar(),
+      body: Center(
+        child: DataTable(
+          columns: [
+            DataColumn(label: Text("Name")),
+            DataColumn(label: Text("Age")),
+            DataColumn(label: Text("Role")),
+          ],
+          rows: [
+            DataRow(
+              cells: [
+                DataCell(Text("Emon")),
+                DataCell(Text("25")),
+                DataCell(Text("Student")),
+              ],
             ),
-            title: Text(user["name"]),
-            subtitle: Text(user["email"]),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsPage(receivedata: user),
-                ),
-              );
-            },
-          );
-        },
+            DataRow(
+              cells: [
+                DataCell(Text("Emon")),
+                DataCell(Text("25")),
+                DataCell(Text("Student")),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
